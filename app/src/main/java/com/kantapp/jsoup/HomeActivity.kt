04e34c00ac7fc05dcee5cb4ecc87143469e65a00
kantapp inc <info@kantapp.com>
2018-04-25 16:56:57 +0530

    Initial commit

---
+++ app/src/main/java/com/kantapp/jsoup/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.kantapp.jsoup
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+@SuppressLint("Registered")
+/**
+ * Created by Android A on 4/25/2018.
+ */
+class HomeActivity:AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
